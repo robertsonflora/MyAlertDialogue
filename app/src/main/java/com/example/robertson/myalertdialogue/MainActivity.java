adfd918650e56fb81e15dd949e003d6890f98ce0
robertsonflora <rpflora@gmail.com>
2017-09-28 15:08:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/robertson/myalertdialogue/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.robertson.myalertdialogue;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openDialog(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Alert!!!")
+                .setCancelable(true)
+                .setTitle("Do you want to close the app?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        builder.create().show();
+    }
+}
